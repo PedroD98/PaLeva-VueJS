400e4406f55577b8bef84d31e5b82b2488dcb854
Pedro Dias <pedroddias98@gmail.com>
2024-11-22 10:35:59 -0300

    Configura API_BASE_URL para interagir com restaurante gerador por seeds.rb

--- orders-list.js
+++ orders-list.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api/v1/restaurants/5FTAWU/orders";
+const API_BASE_URL = "http://localhost:3000/api/v1/restaurants/C4MPU5/orders";
 
 const app = Vue.createApp({
 
